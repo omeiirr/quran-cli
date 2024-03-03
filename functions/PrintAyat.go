a200b5fa1dc49d69d97230d5d70a0a957dc231cf
omeiirr <omeirf.02@gmail.com>
2024-03-04 02:20:06 +0530

    feat: add optional arabic text display

--- functions/PrintAyat.go
+++ functions/PrintAyat.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/charmbracelet/lipgloss"
 	"github.com/omeiirr/quran-cli/data"
+	"github.com/spf13/viper"
 )
 
 func PrintAyat(surahNo int, ayatNo int) {
@@ -20,11 +21,22 @@ func PrintAyat(surahNo int, ayatNo int) {
 			),
 	)
 
-	fmt.Printf(
-		"\n%v\n%v\n",
-		data.QuranPayload[surahNo-1].Verses[ayatNo-1].Text,
-		data.QuranPayload[surahNo-1].Verses[ayatNo-1].Translation,
-	)
+	showArabic := viper.GetBool("print_ayat.show_arabic")
+
+	if showArabic {
+
+		fmt.Printf(
+			"\n%v\n%v\n",
+			data.QuranPayload[surahNo-1].Verses[ayatNo-1].Text,
+			data.QuranPayload[surahNo-1].Verses[ayatNo-1].Translation,
+		)
+	} else {
+
+		fmt.Printf(
+			"\n%v\n",
+			data.QuranPayload[surahNo-1].Verses[ayatNo-1].Translation,
+		)
+	}
 
 	fmt.Printf("\nTafsir: https://quran.com/%v:%v/tafsirs/en-tafisr-ibn-kathir\n", surahNo, ayatNo)
 
