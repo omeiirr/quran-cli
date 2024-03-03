2889fcb5aac3daff85bc0b2bfcfcd59b87fdc008
omeiirr <omeirf.02@gmail.com>
2024-03-04 02:19:10 +0530

    configure viper

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/omeiirr/quran-cli/cmd"
 	"github.com/omeiirr/quran-cli/data"
+	"github.com/spf13/viper"
 
 	_ "embed"
 )
@@ -14,6 +17,14 @@ var chaptersContent []byte
 var quranContent []byte
 
 func init() {
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Unable to read config:", err)
+	}
 	data.LoadData(chaptersContent, quranContent)
 }
 
