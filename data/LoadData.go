41722a307bb31de785d874ae8c4836a2d64d1dd2
omeiirr <omeirf.02@gmail.com>
2024-03-03 16:30:43 +0530

    embed static files in binary

--- data/LoadData.go
+++ data/LoadData.go
@@ -3,7 +3,6 @@ package data
 import (
 	"encoding/json"
 	"log"
-	"os"
 
 	"github.com/omeiirr/quran-cli/models"
 )
@@ -11,22 +10,11 @@ import (
 var ChaptersPayload []models.Surah
 var QuranPayload []models.Surah
 
-func LoadData() {
-	// Load list of all Surahs
-	chaptersContent, err := os.ReadFile("./data/Chapters.json")
-	if err != nil {
-		log.Fatal("Error when opening file: ", err)
-	}
+func LoadData(chaptersContent, quranContent []byte) {
 
-	err = json.Unmarshal(chaptersContent, &ChaptersPayload)
+	err := json.Unmarshal(chaptersContent, &ChaptersPayload)
 	if err != nil {
 		log.Fatal("Error during Unmarshal(): ", err)
-	}
-
-	// Load Quran
-	quranContent, err := os.ReadFile("./data/Quran.json")
-	if err != nil {
-		log.Fatal("Error when opening file: ", err)
 	}
 
 	err = json.Unmarshal(quranContent, &QuranPayload)
--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"github.com/omeiirr/quran-cli/cmd"
 	"github.com/omeiirr/quran-cli/data"
+
+	_ "embed"
 )
 
+//go:embed data/Chapters.json
+var chaptersContent []byte
+
+//go:embed data/Quran.json
+var quranContent []byte
+
 func init() {
-	data.LoadData()
+	data.LoadData(chaptersContent, quranContent)
 }
 
 func main() {
