bd9f14c76aa46de725abb28b73a3dc30288d326c
omeiirr <omeirf.02@gmail.com>
2024-03-05 00:01:37 +0530

    remove unused code

--- functions/PrintSurah.go
+++ functions/PrintSurah.go
@@ -13,13 +13,6 @@ func PrintSurah(surahNo int, showArabic bool) {
 		fmt.Println("Chapter not found; enter a valid chapter number between 1 to 114")
 		return
 	}
-	// if len(verseRange) == 0 {
-	// 	verseRange[0] = 1
-	// 	verseRange[1] = data.ChaptersPayload[surahNo].TotalVerses
-	// }
-
-	// fmt.Println(data.ChaptersPayload[surahNo].TotalVerses)
-	// fmt.Println(verseRange)
 
 	fmt.Println(
 
@@ -37,10 +30,6 @@ func PrintSurah(surahNo int, showArabic bool) {
 			),
 	)
 
-	// fmt.Printf("\nSurah %v", data.QuranPayload[surahNo-1].Translation)
-
-	// fmt.Println(data.QuranPayload[surahNo-1].Verses)
-
 	// Print Id and Translation of each Ayat
 	for _, ayat := range data.QuranPayload[surahNo-1].Verses {
 		if data.Cfg.PrintSurah.ShowArabic || showArabic {
@@ -52,9 +41,4 @@ func PrintSurah(surahNo int, showArabic bool) {
 		}
 	}
 
-	// Print Id and Translation of each Ayat
-	// for i := verseRange[0]; i <= verseRange[1]; i++ {
-	// 	fmt.Printf("%d. %s\n", data.QuranPayload[surahNo].Verses[i].Id, data.QuranPayload[surahNo].Verses[i].Translation)
-
-	// }
 }
